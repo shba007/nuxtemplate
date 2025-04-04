0a339fe37801a3704aac0b5bfb841f63fa7fe8f6
Shirsendu Bairagi <shirsendu2001@gmail.com>
2025-04-04 17:47:40 +0530

    refactor: remove semantic release configuration and related dependencies

--- release.config.js
+++
@@ -1,4 +0,0 @@
-export default {
-  branches: [{ name: 'main' }, { name: 'develop', prerelease: true }],
-  plugins: ['@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator', ['@semantic-release/github', { assets: ['dist/**'] }], '@semantic-release/git'],
-}
